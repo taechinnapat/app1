cef24dde5251bc295125fd59cf61d3b9e73e3cbc
taechinnapat <152370351+taechinnapat@users.noreply.github.com>
2025-01-16 11:35:30 +0700

    Add Even-data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
-import {Calculator2} from "./calculator";
-function App() {
-  return <Calculator2/>
+import { EventData2 } from "./event-data";
+export default function App() {
+  return <EventData2/>
 }
-
-export default App;
