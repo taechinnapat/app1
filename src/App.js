50fb484e6007479faba79bbe9c84e0201785bda8
taechinnapat <152370351+taechinnapat@users.noreply.github.com>
2025-01-16 10:45:43 +0700

    Add function calculator

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Button from "./event-class";
+import {Calculator2} from "./calculator";
 function App() {
-  return <Button/>
+  return <Calculator2/>
 }
 
 export default App;
