56f14e501a304f62e9bccf7730fcb29bb4ab7b5c
taechinnapat <152370351+taechinnapat@users.noreply.github.com>
2025-01-30 09:59:01 +0700

    Create Context and Create context-content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from "react";
-import MessageBox from "./state-func";
-export default function App() {
-  return <MessageBox/>
+import { userContext } from "./context";
+import Content from "./context-content";
+ function App() {
+  return (
+    <userContext.Provider value ={'Tom Jerry'}>
+      <Content/>
+    </userContext.Provider>
+  )
 }
+export default App;
